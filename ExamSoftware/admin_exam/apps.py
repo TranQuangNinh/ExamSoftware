867df521dc8ee3b5db44fc47d3d2431ed20e603d
TranQuangNinh <33461757+TranQuangNinh@users.noreply.github.com>
2019-04-28 16:49:23 +0700

    ExamSoftware

--- ExamSoftware/admin_exam/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AdminExamConfig(AppConfig):
-    name = 'admin_exam'
--- ExamSoftware/admin_exam/send_email.py
+++
@@ -1,6 +0,0 @@
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'examsoftware2019@gmail.com'
-EMAIL_HOST_PASSWORD = 'exam@2019'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
