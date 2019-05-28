5c5a7aa70f3c1dd7f845f584b42698d0f22fd097
Tran Quang Ninh <tranquangninh97@gmail.com>
2019-05-28 17:14:15 +0700

    Update code

--- ExamSoftware/admin_exam/send_email.py
+++ ExamSoftware/admin_exam/send_email.py
@@ -1,6 +1,6 @@
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'examsoftware2019@gmail.com'
-EMAIL_HOST_PASSWORD = 'exam@2019'
+EMAIL_HOST_USER = 'youremail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
